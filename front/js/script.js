695929c891ea1ddfcb7642fc62ff2a863b038050
fantaconde <fantaconde239@gmail.com>
2022-10-23 21:54:04 +0300

    20% complete

---
+++ front/js/script.js
@@ -0,0 +1,34 @@
+
+//Call the API
+fetch("http://localhost:3000/api/products")
+.then(response => response.json())
+//Console log the data and use the data on the HTML Page
+.then(data => {
+    //confirm the data has been received
+    console.log(data);
+
+    //Loop through the data and display it on the HTML page
+    data.forEach(product => {
+        document.querySelector(".items").innerHTML += `
+        
+         <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>
+        `
+    })
+})
+//Return an error if the API call fails
+.catch(error => {
+    console.error(error);
+})
+
+
+// <div class="card">
+        //     <h2>${product.name}</h2>
+        //     <p>${product.price}</p>
+        //     <p>${product.description}</p>
+        // </div>
