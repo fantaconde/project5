f178ed5b5de91681f2fdcb7e0ee17b011a52426b
fantaconde <fantaconde289@gmail.com>
2022-11-01 04:27:35 +0300

    Add Item to Cart

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,5 @@
 //Get the URL of the current page
+//
 var url_str = window.location.href;
 console.log(url_str);
 
@@ -9,51 +10,98 @@ console.log(url);
 //Get the ID from the URL
 var id = url.searchParams.get("id");
 
+//Store Values
+const productDetails = [];
+
 //Console log the ID
 console.log(id);
 
 fetch("http://localhost:3000/api/products/" + id)
-    .then(response => response.json())
-    .then(product => {
-        console.log(product);
-        //insert product image 
-        document.querySelector(".item__img").innerHTML += `
+  .then((response) => response.json())
+  .then((product) => {
+    console.log(product);
+    productDetails.push(product);
+    //insert product image
+    document.querySelector(".item__img").innerHTML += `
          <img src="${product.imageUrl}" alt="Photographie d'un canapé">
-        `
+        `;
 
-        //insert product title
-        document.getElementById('title').innerHTML = `${product.name}`;
+    //insert product title
+    document.getElementById("title").innerHTML = `${product.name}`;
 
-        //Insert product description
-        document.getElementById('description').innerHTML = `${product.description}`;
+    //Insert product description
+    document.getElementById("description").innerHTML = `${product.description}`;
 
-        //get quantity
-        // var quantity = document.getElementById('quantity').value;
-        // var totalprice = product.price * 2;
-        //Insert product price
-        document.getElementById('price').innerHTML = `${product.price}`;
+    //get quantity
+    // var quantity = document.getElementById('quantity').value;
+    // var totalprice = product.price * 2;
+    //Insert product price
+    document.getElementById("price").innerHTML = `${product.price}`;
 
-        //Insert product colors
-        // document.getElementById('colors').innerHTML = `${product.colors}`;
-        product.colors.forEach(color => {
-                document.querySelector("#colors").innerHTML += `
+    //Insert product colors
+    // document.getElementById('colors').innerHTML = `${product.colors}`;
+    product.colors.forEach((color) => {
+      document.querySelector("#colors").innerHTML += `
                 <option value="${color}">${color}</option>
-                `
-            })
-            
+                `;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-    })
-    .catch(error => {
-        console.log(error);
-    })
+function addToCart() {
+  // basket = []
+  const quantity = document.getElementById("quantity").value;
+  const color = document.getElementById("colors").value;
+  const price = productDetails[0].price;
 
+  const basket = {
+    productid: `${id}`,
+    productQuantity: `${quantity}`,
+    productColor: `${color}`,
+    productPrice: `${price}`,
+  };
 
+  //Check if cart is empty
+  if (localStorage.getItem("cart") === null) {
+    //Create cart
+    localStorage.setItem("cart", JSON.stringify([basket]));
+    console.log("cart created");
+    //if cart is not empty
+  } else {
+    //Get cart
+    const cart = JSON.parse(localStorage.getItem("cart"));
 
-    // fetch(url)
-    // .then(response => response.json())
-    // .then(data => {
-    //     //use the data
-    // })
-    // .catch(error => {
-    //     //do anything with the error
-    // });
+    //Check if product is already in cart
+    const productInCart = cart.find(
+        (product) => product.productid === id && product.productColor === color
+        );
+
+    //If product is already in cart
+    if (productInCart) {
+      //Update quantity
+      productInCart.productQuantity =
+        parseInt(productInCart.productQuantity) + parseInt(quantity);
+      console.log(productInCart.productQuantity);
+      //Update cart
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    //if product is not in cart
+    else {
+      //Add product to cart
+      localStorage.setItem("cart", JSON.stringify([...cart, basket]));
+    }
+  }
+}
+
+// addToCart.push(item)
+
+// fetch(url)
+// .then(response => response.json())
+// .then(data => {
+//     //use the data
+// })
+// .catch(error => {
+//     //do anything with the error
+// });
